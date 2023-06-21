c1808b8d73681fc2e0f0c5dde32ca61fa8d2476c
Nielsen Castelo Damasceno Dantas <39357322+nielsencastelo@users.noreply.github.com>
2023-06-21 16:55:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -43,14 +43,23 @@ def synthesize_speech(text, output_file):
     # Reproduzir o arquivo de áudio
     os.system("mpg123 " + output_file)
 
+def remove_header(text, header_text):
+    # Remover o cabeçalho do texto
+    content = text.replace(header_text, "")
+    return content
+
 def main():
     pdf_file = "33674-Article-377528-1-10-20220824.pdf"
     output_file = "output.mp3"
-
+    
+    header = """Research, Society and Development, v. 11, n. 1 1, e349111133674 , 2022  
+(CC BY 4.0) | ISSN 2525 -3409 | DOI: http://dx.doi.org/10.33448/rsd -v11i11.33674 
+    """
     # Ler o PDF
     text = read_pdf(pdf_file)
     text_pre = remove_text_before_intro(text)
     text_pre = remove_references(text_pre)
+    text_pre = remove_header(text_pre, header)
 
     # Sintetizar a voz
     synthesize_speech(text_pre, output_file)
